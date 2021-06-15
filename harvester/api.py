8673013d2cb3ef4dcf6f1c786ec7056c86fff081
Phantomias3782 <andreas@mmacb.de>
2021-06-15 10:13:56 +0200

    added scholar button and functionality. Bug fixes pending

--- harvester/api.py
+++ harvester/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template,jsonify
 from harvester_foodpath import get_work_done
 from harvester_newspap3r import get_it_done
+from harvester_scholar import get_content
 
 app = Flask(__name__)
 
@@ -11,6 +12,10 @@ def do_foodpath(question):
 def do_newspaper(question):
    df =  get_it_done(question)
    return df.to_html(header="true", table_id="newspaper")
+
+def do_scholar(question):
+    df = get_content(question)
+    return df.to_html(header="true", table_id="scholar")
 
 
 @app.route('/')
@@ -32,6 +37,16 @@ app.route('/newspaper', methods=['GET','POST'])
 def my_form_post():
     question = request.form['question']
     resultdf = do_newspaper(question)
+    result = {
+        "output": resultdf
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+
+@app.route('/scholar', methods=['GET','POST'])
+def my_form_post2():
+    question = request.form['question']
+    resultdf = do_scholar(question)
     result = {
         "output": resultdf
     }
---
+++ harvester/harvester_scholar.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import re
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+
+
+def get_url(query):
+    
+    base_url = "https://scholar.google.com/scholar?hl=de&as_sdt=0%2C5&q="
+    
+    for element in query.split():
+        base_url = base_url + element + "+"
+    
+    # return new base url with search keywords
+    return base_url
+
+
+def get_content(query):
+    
+    url = get_url("machine learning")
+
+    response=requests.get(url,headers=headers)
+    soup=BeautifulSoup(response.content,'lxml')
+
+    articles = pd.DataFrame(columns = ['title', 'content', 'url'])
+
+    # loop over articles
+    for item in soup.select("[data-lid"):
+
+        try:
+            
+            # get needed content
+            title = item.select("h3")[0].get_text()
+            url = item.select('a')[0]['href']
+            content = item.select('.gs_rs')[0].get_text()
+
+            # get citation count
+            citation = item.select("a")
+            for element in citation:
+                
+                if "Zitiert" in str(element):
+
+                    citation = re.search(r"(?<=von: )\d+", str(element))[0]
+            
+            # save to dataframe
+            articles.loc[citation] = [title, content, url]
+        
+        except:
+
+            pass
+    
+    return articles
